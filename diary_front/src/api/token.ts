625001da7da71bb97531a59aafefa2f4fe072073
wkdtldn <matteo.jsw@gmail.com>
2024-09-28 10:53:37 +0900

    add csrftoken

--- diary_front/src/api/token.ts
+++ diary_front/src/api/token.ts
@@ -2,6 +2,5 @@ import { api } from "./axiosInstance";
 
 export const fetchCookies = async () => {
   const res = await api.get("/api/token/csrf/");
-  console.log(res.data);
-  return res.data;
+  return res.data.csrftoken;
 };
