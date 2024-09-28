874a38d6c915bba2cc05cbacbe1110e619dd19b5
wkdtldn <matteo.jsw@gmail.com>
2024-09-28 13:42:18 +0900

    reupload

--- drf/back/back/settings.py
+++ drf/back/back/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "main--d-diary.netlify.app",
     "d-diary.netlify.app",
-    "https://db90-121-135-218-25.ngrok-free.app",
+    "db90-121-135-218-25.ngrok-free.app",
 ]
 
 
