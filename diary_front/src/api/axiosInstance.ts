81f32cfc261283e4f5b06e930156aee47a0bca42
wkdtldn <matteo.jsw@gmail.com>
2024-09-28 13:43:29 +0900

    rewrite request address

--- diary_front/src/api/axiosInstance.ts
+++ diary_front/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://cc84-121-135-218-25.ngrok-free.app",
+  baseURL: "https://db90-121-135-218-25.ngrok-free.app",
   withCredentials: true,
 });
