11abf54e805440fb604d73e933075508701d361f
wkdtldn <matteo.jsw@gmail.com>
2024-09-28 13:17:46 +0900

    header footer css

--- diary_front/src/components/Layout/Header/Header.tsx
+++ diary_front/src/components/Layout/Header/Header.tsx
@@ -3,28 +3,24 @@ import Menu from "../../Menu/Menu";
 import "./Header.css";
 import React, { useState } from "react";
 import { LoginUser } from "../../../hooks/recoil/userState";
+import { IoMenu, IoClose } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   const [menuOn, setMenuOn] = useState<boolean>(false);
 
   const login_user = useRecoilValue(LoginUser);
 
   return (
     <header className="Header">
-      <button
-        className="header-btn"
-        title="뒤로가기"
-        onClick={() => window.history.back()}
-      >
-        뒤로가기
+      <button className="header-btn" onClick={() => setMenuOn(!menuOn)}>
+        {menuOn ? <IoClose /> : <IoMenu />}
       </button>
       <span className="header-title">{login_user.name}의 일기장</span>
-      <button
-        className="header-btn"
-        title={menuOn ? "메뉴 닫기" : "메뉴 열기"}
-        onClick={() => setMenuOn(!menuOn)}
-      >
-        {menuOn ? "메뉴 닫기" : "메뉴"}
+      <button className="header-btn" onClick={() => navigate("/profile")}>
+        내 정보
       </button>
       <Menu on={menuOn} status={() => setMenuOn(false)} />
     </header>
