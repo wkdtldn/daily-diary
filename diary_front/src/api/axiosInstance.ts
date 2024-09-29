a83a8178d4ca3359c8bd0d16c62d17b11bc9d06f
wkdtldn <matteo.jsw@gmail.com>
2024-09-29 16:16:09 +0900

    fix global request address

--- diary_front/src/api/axiosInstance.ts
+++ diary_front/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://6085-121-135-218-25.ngrok-free.app",
+  baseURL: "https://ecfd-121-135-218-25.ngrok-free.app",
   withCredentials: true,
 });
