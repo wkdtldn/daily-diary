23fa2c5703c081b0d04a2762d52377365cc43f05
wkdtldn <matteo.jsw@gmail.com>
2024-09-29 13:32:28 +0900

    rm alert func res

--- diary_front/src/api/token.ts
+++ diary_front/src/api/token.ts
@@ -2,6 +2,5 @@ import { api } from "./axiosInstance";
 
 export const fetchCookies = async () => {
   const res = await api.get("/api/token/csrf/");
-  alert(res.data);
   return res.data.csrftoken;
 };
