3da3910a3a2a9d3c5c34775cacea16bb770c8578
wkdtldn <matteo.jsw@gmail.com>
2024-11-03 20:53:00 +0900

    add skip ngrok cors

--- diary_front/src/api/axiosInstance.ts
+++ diary_front/src/api/axiosInstance.ts
@@ -9,7 +9,7 @@ export const api = axios.create({
   baseURL: "https://3a53-221-149-135-202.ngrok-free.app",
   headers: {
     "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "69420",
+    "ngrok-skip-browser-warning": true,
   },
   withCredentials: true,
 });
